88493348085d86c61d8f7eecf3d089e1b7d51b0d
Carlos <chzelaya1201@gmail.com>
2022-08-29 20:34:15 -0600

    Commiting progress

---
+++ server.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const database = {
+    users: [
+        {
+            id: "123",
+            name: "John",
+            password: "cookies",
+            email: "john@example.com",
+            entries: 0,
+            joined: new Date(),
+        },
+        {
+            id: "124",
+            name: "Sally",
+            password: "bananas",
+            email: "Sally@example.com",
+            entries: 0,
+            joined: new Date(),
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@example.com',
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.json(database.users);
+});
+
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+        res.json('success')
+    } else {
+        res.status(400).json('error')
+    }
+});
+
+app.post('/register', (req, res) => {
+    const { email, password, name } = req.body;
+    database.users.push({
+        id: "125",
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date(),
+    })
+    res.json(database.users[database.users.length - 1])
+});
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json("user not found");
+    }
+})
+
+app.post("/image", (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json("user not found");
+    }
+})
+
+
+// bcrypt.hash(password, null, null, function (err, hash) {
+//     console.log("hashed password:", hash);
+// });
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
+
+
+
+
+
+
+
+
+
