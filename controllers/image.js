3e27e88603cfa6349c9d653bcd866c863e734d1a
Carlos <chzelaya1201@gmail.com>
2022-09-05 14:05:36 -0600

    committing latest changes

---
+++ controllers/image.js
@@ -0,0 +1,68 @@
+const Clarifai = require('clarifai');
+const { request, response } = require('express');
+
+
+
+const app = new Clarifai.App({
+    apiKey: '1452858574db4606bd2e499b122d97cb'
+})
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json("unable to work with api")
+        })
+}
+
+
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0].entries)
+        })
+        .catch(err => res.status(404).json("unable to get entries", err.message))
+}
+
+
+//! Holding on to this code while figuring out backend api call.
+// handleApiCall = (req, res, fetch) => {
+//     const raw = JSON.stringify({
+//         "user_app_id": {
+//             "user_id": 'bzb0xw4yumac',
+//             "app_id": 'ZTM-Face-Recognition'
+//         },
+//         "inputs": [
+//             {
+//                 "data": {
+//                     "image": {
+//                         "url": req.body.input
+//                     }
+//                 }
+//             }
+//         ]
+//     });
+//     const requestOptions = {
+//         method: 'POST',
+//         headers: {
+//             'Accept': 'application/json',
+//             'Authorization': 'Key ' + '01ba72f5e58f4a4b8afa0f86439cf98f'
+//         },
+//         body: raw
+//     };
+
+//     fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/versions/" + "6dc7e46bc9124c5c8824be4822abe105" + "/outputs", requestOptions)
+//         .then(response => response.json())
+// };
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
+const fetch = require('node-fetch');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const image = require('./controllers/image');
+const profile = require('./controllers/profile');
+const imageurl = require('./controllers/image');
+
+
 
 const db = knex({
     client: 'pg',
@@ -15,81 +23,19 @@ const db = knex({
     }
 });
 
-db.select('*').from('users')
-
-// .then(data => {
-//     console.log(data);
-// })
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) });
 
-app.get('/', (req, res) => {
-    res.json(database.users);
-});
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) });
 
-app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('error')
-    }
-});
+app.put("/image", (req, res) => { (image.handleImage(req, res, db)) });
+app.post("/imageurl", (req, res) => { (image.handleApiCall(req, res,)) });
 
-app.post('/register', (req, res) => {
-    const { email, name } = req.body;
-    db('users')
-        .returning("*")
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date()
-        }).then(user => {
-            res.json(user[0]);
-        })
-        .catch(err => res.status(400).json('unable to register'))
-});
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({
-        id: id,
-    })
-        .then(user => res.json(user[0]));
-    // if (!found) {
-    //     res.status(400).json("user not found");
-    // }
-})
-
-app.post("/image", (req, res) => {
-    const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    })
-    if (!found) {
-        res.status(400).json("user not found");
-    }
-})
-
-
-// bcrypt.hash(password, null, null, function (err, hash) {
-//     console.log("hashed password:", hash);
-// });
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
